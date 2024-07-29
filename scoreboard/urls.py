9bee558ac7efb99deae691cf16512960a44705c2
oscarlincoln <okumuoscar1712@gmail.com>
2024-07-29 11:55:55 +0300

    push

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -4,7 +4,7 @@ from .views import ScoreList, get_top_scores, ScoresAboveThresholdView, ScoresBe
 urlpatterns = [
     path('scores/', ScoreList.as_view(), name='score-list'),
     path('scores/top/<int:top_n>/', get_top_scores, name='top-scores'),
-    path('scores/above/<float:threshold>/', ScoresAboveThresholdView.as_view(), name='scores-above-threshold'),
-    path('scores/below/<float:threshold>/', ScoresBelowThresholdView.as_view(), name='scores-below-threshold'),
+    path('scores/above_threshold/<float:threshold>/', ScoresAboveThresholdView.as_view(), name='scores-above-threshold'),
+    path('scores/below_threshold/<float:threshold>/', ScoresBelowThresholdView.as_view(), name='scores-below-threshold'),
     path('scores/range/<int:start>/<int:end>/', get_scores_in_range, name='scores-in-range'),
 ]
