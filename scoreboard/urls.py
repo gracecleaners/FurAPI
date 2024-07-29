6881530fd42932a25daf9ebc95f4f4309ad56713
oscarlincoln <okumuoscar1712@gmail.com>
2024-07-29 13:37:46 +0300

    push

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ScoreList, get_top_scores, ScoresAboveThresholdView, ScoresBelowThresholdView, get_scores_in_range
+from .views import ScoreList, get_top_scores, get_scores_above_threshold, get_scores_below_threshold, get_scores_in_range
 
 urlpatterns = [
     path('scores/', ScoreList.as_view(), name='score-list'),
     path('scores/top/<int:top_n>/', get_top_scores, name='top-scores'),
-    path('scores/above_threshold/<float:threshold>/', ScoresAboveThresholdView.as_view(), name='scores-above-threshold'),
-    path('scores/below_threshold/<float:threshold>/', ScoresBelowThresholdView.as_view(), name='scores-below-threshold'),
+    path('scores/above_threshold/<float:threshold>/', get_scores_above_threshold, name='scores-above-threshold'),
+    path('scores/below_threshold/<float:threshold>/', get_scores_below_threshold, name='scores-below-threshold'),
     path('scores/range/<int:start>/<int:end>/', get_scores_in_range, name='scores-in-range'),
 ]
--- scoreboard/views.py
+++ scoreboard/views.py
@@ -3,44 +3,33 @@ from .models import Score
 from .serializers import ScoreSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
-from rest_framework import status
+
 
 class ScoreList(generics.ListCreateAPIView):
     queryset = Score.objects.all()
     serializer_class = ScoreSerializer
 
-class ScoresBelowThresholdView(APIView):
-    def get(self, request, threshold):
-        try:
-            threshold = float(threshold)
-        except ValueError:
-            return Response({"error": "Invalid threshold value"}, status=status.HTTP_400_BAD_REQUEST)
 
-        scores = Score.objects.filter(survive_seconds__lt=threshold).order_by('-survive_seconds')
-        serializer = ScoreSerializer(scores, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
+@api_view(['GET','OPTIONS'])
 def get_top_scores(request, top_n):
     top_scores = Score.objects.all().order_by('-survive_seconds')[:top_n]
     serializer = ScoreSerializer(top_scores, many=True)
     return Response(serializer.data)
 
-class ScoresAboveThresholdView(APIView):
-    def get(self, request, threshold):
-        try:
-            threshold = float(threshold)
-        except ValueError:
-            return Response({"error": "Invalid threshold value"}, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET','OPTIONS'])
+def get_scores_above_threshold(request, threshold):
+    scores = Score.objects.filter(survive_seconds__gt=threshold).order_by('-survive_seconds')
+    serializer = ScoreSerializer(scores, many=True)
+    return Response(serializer.data)
 
-        scores = Score.objects.filter(survive_seconds__gt=threshold).order_by('-survive_seconds')
-        serializer = ScoreSerializer(scores, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+@api_view(['GET','OPTIONS'])
+def get_scores_below_threshold(request, threshold):
+    scores = Score.objects.filter(survive_seconds__lt=threshold).order_by('-survive_seconds')
+    serializer = ScoreSerializer(scores, many=True)
+    return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(['GET','OPTIONS'])
 def get_scores_in_range(request, start, end):
     scores = Score.objects.all().order_by('-survive_seconds')[start:end]
     serializer = ScoreSerializer(scores, many=True)
